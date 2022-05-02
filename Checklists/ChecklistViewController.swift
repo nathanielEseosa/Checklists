a7f5456a66394bf460dd5f3f320c891d15ca9349
Nathaniel <eseos@icloud.com>
2022-05-02 23:07:05 +0200

    The first iteration

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    let row0text = "Walk the dog"
+    let row1text = "Brush teeth"
+    let row2text = "Learn iOS development"
+    let row3text = "Soccer practice"
+    let row4text = "Eat ice cream"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,15 +37,15 @@ class ChecklistViewController: UITableViewController {
         let label = cell.viewWithTag(1000) as! UILabel
 
           if indexPath.row == 0 {
-            label.text = "Walk the dog"
+            label.text = row0text
           } else if indexPath.row == 1 {
-            label.text = "Brush my teeth"
+            label.text = row1text
           } else if indexPath.row == 2 {
-            label.text = "Learn iOS development"
+            label.text = row2text
           } else if indexPath.row == 3 {
-            label.text = "Soccer practice"
+            label.text = row3text
           } else if indexPath.row == 4 {
-            label.text = "Eat ice cream"
+            label.text = row4text
           }
 
         return cell
