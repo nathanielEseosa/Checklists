5043379547da6ad3db54a8f0393bbcc5332e010b
Nathaniel <eseos@icloud.com>
2022-05-20 00:01:24 +0200

    Add remove item logic

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -93,4 +93,11 @@ class ChecklistViewController: UITableViewController {
         }
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        items.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
 }
